1086530733f992efc51cc810e1dab3e4a5b3d43d
Zhijian Xia <xiazhijian@vpgame.cn>
2018-03-22 17:30:57 +0800

    Add getMatchDetails interface
    - team info for match, match time, the event belong to
    - round scores
    - player match stats
    - player round stats(to be implemented)
    
    Signed-off-by: Zhijian Xia <xiazhijian@vpgame.cn>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Results from './results';
 import RSS from './rss';
 import Matches from './matches';
+import MatchDetail from './match_detail';
 
 export const getNews = (cb) => new RSS('news', cb);
 export const getResults = (cb) => new Results(cb);
 export const getMatches = (id, cb) => new Matches(id, cb);
+export const getMatchDetails = (id, cb) => new MatchDetail(id, cb);
