e877847f3528121b5d78c11066fdf68f4b5dc642
Zhijian Xia <isword.xia@gmail.com>
2018-04-05 14:43:25 +0800

    Add try catch for result2
    Signed-off-by: Zhijian Xia <isword.xia@gmail.com>

--- src/results2.js
+++ src/results2.js
@@ -4,7 +4,7 @@ import { CONFIG } from './config';
 
 /**
  * Scraping results
- * 
+ *
  * @export
  * @class Results
  */
@@ -12,56 +12,60 @@ export default class Results2 {
 
   /**
    * Creates an instance of Results.
-   * 
-   * @param {any} callback 
-   * 
+   *
+   * @param {any} callback
+   *
    * @memberOf Results
    */
   constructor(offset, callback) {
     const uri = `${CONFIG.BASE}${CONFIG.RESULTS}?offset=${offset}`;
 
     request({ uri }, (error, response, body) => {
-      const $ = cheerio.load(body, {
-        normalizeWhitespace: true
-      });
+      try {
+        const $ = cheerio.load(body, {
+          normalizeWhitespace: true
+        });
 
-      const resultElements = $('.results-all .result-con');
+        const resultElements = $('.results-all .result-con');
 
-      let results = [];
+        let results = [];
 
-      $(resultElements).each((i, element) => {
-        const el = $(element).find('tr');
-        const team1 = el.children('.team-cell').first();
-        const team2 = el.children('.team-cell').last();
-        const matchId = $(element).children('a').attr('href');
-        const matchTime = parseInt($(element).attr('data-zonedgrouping-entry-unix'));
-        const maps = el.find('.map');
-        const result1 = el.find('.result-score').children('span').first();
-        const result2 = el.find('.result-score').children('span').last();
+        $(resultElements).each((i, element) => {
+          const el = $(element).find('tr');
+          const team1 = el.children('.team-cell').first();
+          const team2 = el.children('.team-cell').last();
+          const matchId = $(element).children('a').attr('href');
+          const matchTime = parseInt($(element).attr('data-zonedgrouping-entry-unix'));
+          const maps = el.find('.map');
+          const result1 = el.find('.result-score').children('span').first();
+          const result2 = el.find('.result-score').children('span').last();
 
-        const objData = {
-          match_time: matchTime ? matchTime/1000 : 0,
-          event: el.find('.event-name').text(),
-          maps: maps.text(),
-          team1: {
-            name: team1.find('.team').text(),
-            crest: team1.find('img').attr('src'),
-            result: parseInt(result1.text()),
-            is_win: result1.hasClass('score-won')
-          },
-          team2: {
-            name: team2.find('.team').text(),
-            crest: team2.find('img').attr('src'),
-            result: parseInt(result2.text()),
-            is_win: result2.hasClass('score-won')
-          },
-          matchId
-        };
+          const objData = {
+            match_time: matchTime ? matchTime/1000 : 0,
+            event: el.find('.event-name').text(),
+            maps: maps.text(),
+            team1: {
+              name: team1.find('.team').text(),
+              crest: team1.find('img').attr('src'),
+              result: parseInt(result1.text()),
+              is_win: result1.hasClass('score-won')
+            },
+            team2: {
+              name: team2.find('.team').text(),
+              crest: team2.find('img').attr('src'),
+              result: parseInt(result2.text()),
+              is_win: result2.hasClass('score-won')
+            },
+            matchId
+          };
 
-        results.push(objData);
-      });
+          results.push(objData);
+        });
 
-      callback(results, error);
+        callback(results, error);
+      } catch (e) {
+          callback([], new Error('Parse match result failed'));
+      }
     });
   }
 }
